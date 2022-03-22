08877ea3d4c2f9d12ea54005a5ee59b0bc228d54
joamessina <joaquin.messina@gmail.com>
2022-03-22 17:29:44 -0300

    Creacion modulo register contra Firebase

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,52 @@
+import {useState} from 'react'
+import {useAuth} from '../context/authContext'
+import {useNavigate} from 'react-router-dom'
+
 export function Register() {
+
+  const [user, setuser] = useState({
+    email: '',
+    password: '',
+  })
+
+  const {signup} = useAuth()
+  const navigate = useNavigate()
+  const [error, setError] = useState()
+
+  const handleChange = ({target : {name, value }}) => {
+    setuser({...user ,[name]: value})
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setError('')
+    try {
+      
+      await signup(user.email, user.password)
+      navigate('/')
+    } catch (error) {
+      setError(error.message)
+    }
+  }
+
   return (
     <div>
-      Register
+
+      {error && <p>{error}</p>}
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" placeholder="youremail@mail.com"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="password">password</label>
+        <input type="password" name="password" id="password" 
+          onChange={handleChange}
+        />
+        
+        <button>Register</button>
+      </form>
     </div>
   )
 }
